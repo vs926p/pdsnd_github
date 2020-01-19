a30ec74bcf9046c65a12507d1c8700b6d8ea6b53
Shah, Vivek (vs926p) <vs926p@us.att.com>
2020-01-18 19:15:36 -0500

    Refactored display_data function in bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -198,7 +198,7 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
-        display_data(df)
+        #display_data(df)
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
